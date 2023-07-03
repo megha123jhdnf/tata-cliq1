60fdfe37ec049235a97680fe0e2e8e36aa800d07
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-07-03 22:29:21 +0530

    update

--- myproject1/src/component/Navbar.jsx
+++ myproject1/src/component/Navbar.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 function Navbar(){
-    return(
-        <div style="border:1px solid red;">
-             <h1>Login</h1>
-        </div>
-    )
+    class MyHeader extends React.Component {
+        render() {
+          return (
+            <div>
+               <h1>navi</h1>
+
+            </div>
+          );
+        }
+      }
+      const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<MyHeader />);
 }
 export default Navbar;
