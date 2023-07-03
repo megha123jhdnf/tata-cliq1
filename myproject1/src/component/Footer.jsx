ed9ddac0e8877a32598d690c48cc168404dd74a4
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-07-03 18:40:09 +0530

    update

---
+++ myproject1/src/component/Footer.jsx
@@ -0,0 +1,8 @@
+function Navbar(){
+    return(
+        <div>
+            <h1>this is footer</h1>
+        </div>
+    )
+}
+export default Navbar;
---
+++ myproject1/src/component/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar(){
+    return(
+        <div style="border:1px solid red;">
+             <h1>Login</h1>
+        </div>
+    )
+}
+export default Navbar;
