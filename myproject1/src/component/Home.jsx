e974af880753487bb75358430a7a541862faa167
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-07-07 16:09:53 +0530

    update

---
+++ myproject1/src/component/Home.jsx
@@ -0,0 +1,254 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+
+    <>
+      <div className='maincontainer'>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855656478.jpg' alt=''/>
+      </div>
+
+      <div id='quick-procducts'>
+        <div className='quick-procducts-align'>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596848218142.jpg'alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47575793303582.jpg'alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg'alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg'alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg' alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466546206.jpg'alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466611742.jpg' alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466611742.jpg' alt=''/>
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47548678373406.jpg'alt='' />
+          </div>
+          <div className='quick-procducts-card'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466152990.jpg' alt=''/>
+          </div>
+        </div>
+      </div>
+
+      <div id='bank-banner'>
+        <div className='bank-banner-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/46762762436638.jpg' alt='' />
+        </div>
+        <div className='bank-banner-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855787550.jpg'alt='' />
+        </div>
+        <div className='bank-banner-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/46690782937118.jpg'  alt=''/>
+        </div>
+      </div> 
+      {/* only for title background  */}
+      <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344408094.jpg' alt=''/>
+        </div>
+      </div>
+      {/* for product showing  */}
+      <div className='ondemand-products'>
+        <div className='ondemand-products-card'>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596857851934.jpg' alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865257502.jpg' alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596857983006.jpg'alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596866830366.jpg' alt='' />
+          </div>
+        </div>
+      </div>
+      {/* only for title background  */}
+      <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/45993134948382.jpg'  alt=''/>
+        </div>
+      </div>
+      {/* for product showing  */}
+      <div className='ondemand-products'>
+        <div className='ondemand-products-card'>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596650758174.jpg' alt=''/>
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853493790.jpg' alt=''/>
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853690398.jpg'alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865323038.jpg' alt=''/>
+          </div>
+        </div>
+      </div>
+      {/* only for title background  */}
+      <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344277022.jpg'alt='' />
+        </div>
+      </div>
+      {/* for product showing with circle child div  */}
+
+      <div className='ondemand-products-card'>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596650758174.jpg' alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853493790.jpg' alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853690398.jpg' alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865323038.jpg' alt='' />
+          </div>
+        </div>
+
+
+
+         {/* only for title background  */}
+      <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/45993137373214.jpg' alt='' />
+        </div>
+      </div>
+
+
+       {/* for product showing  */}
+       <div className='ondemand-products'>
+        <div className='ondemand-products-card'>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596650758174.jpg' alt=''/>
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853493790.jpg'alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853690398.jpg' alt='' />
+          </div>
+          <div className='ondemand-products-card-imageHolder'>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865323038.jpg' alt='' />
+          </div>
+        </div>
+      </div>
+
+          {/* *********Feature Brands*********** */}
+          <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/45993134948382.jpg'  alt=''/>
+        </div>
+      </div>
+
+
+
+          <div className='feature-brands'>
+            <div className='feature-Brands'>
+              <div>
+                <img src='https://assets.tatacliq.com/medias/sys_master/images/47627124441118.jpg' alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47627124506654.jpg' alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47627124572190.jpg' alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47627124637726.jpg' alt=''/>
+              </div>
+
+            </div>
+
+          </div>
+                       {/* *********hair corner********* */}
+
+          <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47636183449630.jpg' alt=''/>
+        </div>
+      </div>
+
+
+
+          <div className='feature-brands'>
+            <div className='feature-Brands'>
+              <div>
+                <img src='https://assets.tatacliq.com/medias/sys_master/images/46926359527454.jpg'alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/46926359658526.jpg' alt=''/>
+              </div>
+              
+
+            </div>
+
+          </div>
+
+
+
+          <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344277022.jpg' alt=''/>
+        </div>
+      </div>
+
+
+          <div className='maincontainer'>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/46904782979102.jpg'alt='' />
+      </div>
+
+
+
+      <div className='title-image-backgroung'>
+        <div className='title-image-backgroung-imageHolder'>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344277022.jpg' alt='' />
+        </div>
+      </div>
+
+
+
+          <div className='feature-brands'>
+            <div className='feature-Brands'>
+              <div>
+                <img src='https://assets.tatacliq.com/medias/sys_master/images/47603263438878.jpg' alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47603263504414.jpg' alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47603263569950.jpg' alt=''/>
+              </div>
+              <div>
+              <img src='https://assets.tatacliq.com/medias/sys_master/images/47603264159774.jpg' alt=''/>
+              </div>
+
+            </div>
+
+          </div>
+     
+   
+    </>
+
+  )
+
+}
+
+export default Home;
--- myproject1/src/component/Menwear.jsx
+++ myproject1/src/component/Menwear.jsx
@@ -1,11 +1,198 @@
-import React from "react";
+import React from 'react'
+import './Menwear.css'
+
 const Menwear = () => {
-    return(
-        <>
-        <div id='Menwear'>
-            <img src="https://assets.tatacliq.com/medias/sys_master/images/47513566183454.jpg" alt="" />
+  return (
+<>
+<div className='mensection'>
+    <img src='https://assets.tatacliq.com/medias/sys_master/images/47513566183454.jpg' alt=''/>
+</div>
+<div id='divfirstimage'>
+    <div id='divchild'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47396042702878.jpg' alt=''/>
         </div>
-        </>
-    )
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396042768414.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396042833950.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396042899486.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396042965022.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396043030558.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396043096094.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396043161630.jpg' alt=''/>
+        </div>
+
+    </div>
+
+</div>
+<div id='divsecondimag'>
+    <div id='divchild1'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47636191477790.jpg' alt=''/>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855787550.jpg' alt=''/>
+        </div>
+    </div>
+</div>
+<div className='topdeal'>
+    <div>
+<img src='https://assets.tatacliq.com/medias/sys_master/images/47396044210206.jpg' alt=''/>
+</div>
+</div>
+
+<div className='bestseller'>
+    <div className='bestsellerimage'>
+    <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396043948062.jpg' alt=''/>
+    </div>
+    <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044013598.jpg' alt=''/>
+    </div>
+    <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044079134.jpg' alt=''/>
+    </div>
+    <div>
+    <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044144670.jpg' alt=''/>
+    </div>
+    </div>
+</div>  
+          {/* offfer************* */}
+
+<div className='topdeal'>
+    <div>
+<img src='https://assets.tatacliq.com/medias/sys_master/images/47396043882526.jpg' alt=''/>
+</div>
+</div>
+<div className='topoffers'>
+    <div className='topoffer'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044275742.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044341278.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044406814.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044472350.jpg' alt=''/>
+        </div>
+    </div>
+
+</div>
+   {/* ======offers image  ======= */}
+
+<div className='topdeal'>
+    <div>
+<img src='https://assets.tatacliq.com/medias/sys_master/images/47396044865566.jpg' alt=''/>
+</div>
+</div>
+
+                          
+<div className='topoffers'>
+    <div className='topoffer'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044931102.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044931102.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396044996638.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396045127710.jpg' alt=''/>
+        </div>
+    </div>
+
+</div>
+             {/* ********level up your game***** */}
+             <div className='topdeal'>
+    <div>
+<img src='https://assets.tatacliq.com/medias/sys_master/images/47396048699422.jpg' alt=''/>
+</div>
+</div>
+<div className='levelup'>
+    <div className='leveup-your-game'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47396048764958.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396048830494.jpg' alt=''/>
+        </div>
+
+    </div>
+
+</div>
+
+  {/* ********stylish addd******** */}
+
+  <div className='topdeal'>
+    <div>
+<img src='https://assets.tatacliq.com/medias/sys_master/images/47396049420318.jpg' alt=''/>
+</div>
+</div>
+
+<div className='topoffers'>
+    <div className='topoffer'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47396049485854.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396049551390.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396049616926.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47396049682462.jpg' alt=''/>
+        </div>
+    </div>
+
+</div>
+
+
+         {/* ****************values store************ */}
+
+         
+<div className='topdeal'>
+    <div>
+<img src='https://assets.tatacliq.com/medias/sys_master/images/47403940184094.jpg' alt=''/>
+</div>
+</div>
+
+                          
+<div className='topoffers'>
+    <div className='topoffer'>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47403939921950.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47403939987486.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47403940053022.jpg' alt=''/>
+        </div>
+        <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47403940118558.jpg' alt=''/>
+        </div>
+    </div>
+
+</div>
+</>
+
+  )
 }
+
 export default Menwear;
