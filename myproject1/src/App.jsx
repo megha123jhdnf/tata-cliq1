503ebc4140374d97a38a2b4d25e10567ba9555e2
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-07-09 10:52:43 +0530

    update

--- myproject1/src/App.jsx
+++ myproject1/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './component/Navbar';
 import Footer from './component/Footer';
 import Menwear from './component/Menwear';
 import MenMultiple from './component/MenMultiple';
+import MenSingle from './component/MenSingle';
 
 function App() {
   console.log("Inside app")
@@ -13,7 +14,7 @@ function App() {
     <div>
     <Navbar/>
     <Routes>
-    
+       <Route exact path='men-single' element={<MenSingle />} />
       <Route exact path='/home' element={<Home/>}/>
       <Route exact path='menwear' element={< Menwear/>} />
       <Route exact path='Men-multiple-product' element={<MenMultiple/>} />
---
+++ myproject1/src/component/MenSingle.jsx
@@ -0,0 +1,238 @@
+import React from "react";
+import './MenSingle.css'
+const MenSingle = () =>{
+    return(
+       <>
+       <div id='menSinglePro'>
+        <div id='left'>
+            <div>
+                <img src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000015584427_437Wx649H_202212060011461.jpeg" alt="" />
+                <img src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000015584427_437Wx649H_202212060011352.jpeg" alt="" />
+
+            </div>
+            <div>
+            <img src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000015584427_437Wx649H_202212060011423.jpeg" alt="" />
+            <img src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000015584427_437Wx649H_202212060011394.jpeg" alt="" />
+            <img src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000015584427_437Wx649H_202212060011455.jpeg" alt="" />
+
+            </div>
+        </div>
+        <div id='right'>
+            <div>
+            <p>U.S.Polo Assn.</p>
+            <p>U.S. Polo Assn. Brown Cotton Regular Fit Printed Polo T-Shirt</p>
+            </div>
+
+            <div>
+                <p>₹<span>1264</span> MRP:<del>₹2299</del> <span id='off'>45% Off</span> </p>
+                <p>Inclusive of all taxes</p>
+            </div>
+            
+            <div>
+                <img src="https://www.tatacliq.com/src/assets/ProductNudgeIcons/PriceDrop.svg" alt="" />
+                <p>Price dropped by <span>₹460</span> in the last <span>2</span>  days</p>
+            </div>
+
+            <div>
+                <p>Use MENSEOSS coupon to get 10% off on cart value 1999/- and above.
+</p>
+            </div>
+
+            <div>
+                <div>
+                    <p>5</p>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div>
+                    <p><span>1</span> Rating</p>
+                </div>
+            </div>
+            <div>
+                <p><span>Colour</span> : Brown</p>
+                <div>
+                    <img src="https://img.tatacliq.com/images/i8/97Wx144H/MP000000015584431_97Wx144H_202212060011421.jpeg" alt="" />
+                    <img src="https://img.tatacliq.com/images/i8/97Wx144H/MP000000015584427_97Wx144H_202212060011461.jpeg" alt="" />
+                    <img src="https://img.tatacliq.com/images/i8/97Wx144H/MP000000015584449_97Wx144H_202212060012511.jpeg" alt="" />
+                    <img src="https://img.tatacliq.com/images/i8/97Wx144H/MP000000015584438_97Wx144H_202212060012001.jpeg" alt="" />
+
+                </div>
+            </div>
+            <div>
+                <div>
+                <p>Select Size</p>
+                <p>Size Guide</p>
+                </div>
+
+                <div>
+                    <p>X</p>
+                    <p>M</p>
+                    <p>L</p>
+                    <p>XL</p>
+                    <p>XXL</p>
+
+                </div>
+                 <p id='model-decrip'>Model is 6'0"/185 cms and is wearing size M</p>
+                 <p id='wash'>Cotton, Machine Wash</p>
+            </div>
+            <div>
+                <p>Available Offers</p>
+                <div>
+                    <div class='offer'>
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/27678831411230.png" alt="" />
+                    <div>
+                        <p>10% Instant Discount on Kotak Bank Credit Cards only. </p>
+                        <p>Min Purchase: ₹2500 Max Discount: ₹1000 <span> more</span></p>
+                    </div>
+                    </div>
+
+                    <div class='offer'>
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/27678831280158.png" alt="" />
+                    <div>
+                        <p>15% off on ICICI Credit Cards </p>
+                        <p>Use Code: ICICIWEEKEND|Min Purchase: ₹1500 <span> more</span></p>
+                    </div>
+                    </div>
+
+                    <div class='offer'>
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/27678831280158.png" alt="" />
+                    <div>
+                        <p>15% off on ICICI Netbanking </p>
+                        <p>Use Code: ICICINB|Min Purchase: ₹2000<span> more</span></p>
+                    </div>
+                    </div>
+                    <p id='offers'>See 5 More Offers</p>
+                    </div>
+
+                    <p>From The Brand</p>
+
+                    <div id='brand'>
+                        <div>
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/34838208610334.png" alt="" />
+                            <p>U.S.Polo Assn.</p>
+                        </div>
+                        <p>U.S.Polo Assn.</p>
+
+                        <div id='visitStore'>
+                            <p>Visit Store</p>
+                        </div>
+                    </div>
+
+                    <div id='cartforProduct'>
+                    <i class="fa-solid fa-share-nodes"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <div >
+                    <p>Buy Now</p>
+                    </div>
+                    <div id='addbag'>
+                    <p>Add to BAG</p>
+
+                    </div>
+                   
+                    </div>
+
+                   
+            </div>
+
+        </div>
+       </div>
+       <div id='forRecommend'>
+        <p>Style It With</p>
+        <div>
+            <p>Recommended Combos</p>
+            <p>Bottom Wear</p>
+            <p>Outer Wear</p>
+            <p>Bags & Wallets</p>
+        </div>
+        <div>
+            <div class='productDecript'>
+                <img src="https://img.tatacliq.com/images/i7/252Wx374H/MP000000010977221_252Wx374H_202110211435581.jpeg" alt="" />
+                <p>Samsung</p>
+                <p class='sameStyle'>Samsung Galaxy watch 4SM-</p>
+                <p class='sameStyle'>R87ONKAINU 44mm...</p>
+                <p>₹<span>12400</span><del>₹29999</del> <span>59% off</span> </p>
+                <div class='starwithno'>
+                    <p>5</p>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class='addtoBag'>
+                    <p>Add to Bag</p>
+                </div>
+            </div>
+            <div class='productDecript'>
+                <img src="https://img.tatacliq.com/images/i10/252Wx374H/MP000000016239600_252Wx374H_202303031238191.jpeg" alt="" />
+                <p>Samsung</p>
+                <p class='sameStyle'>Samsung Galaxy Buds2,active-</p>
+                <p class='sameStyle'>Noise Concellation Earbuds...</p>
+                <p>₹<span>5685</span><del>₹13990</del> <span>59% off</span> </p>
+                <div class='starwithno'>
+                    <p>5</p>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class='addtoBag'>
+                    <p>Add to Bag</p>
+                </div>
+
+            </div>
+            <div class='productDecript'>
+                <img src="https://img.tatacliq.com/images/i7/252Wx374H/MP000000007140590_252Wx374H_202108032042161.jpeg" alt="" />
+                <p>Apple</p>
+                <p class='sameStyle'>Apple 1m USB to Lightning-</p>
+                <p class='sameStyle'>Cable</p>
+                <p>₹<span>1679</span><del>₹1800</del> <span>7% off</span> </p>
+                <div class='starwithno'>
+                    <p>5</p>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class='addtoBag'>
+                    <p>Add to Bag</p>
+                </div>
+            </div>
+        </div>
+        <p id='more'>More From U.S. Polo Assn.</p>
+
+        <div class='moreFromUS'>
+           <div>
+            <img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017735688_437Wx649H_202305281640201.jpeg" alt="" />
+           
+            <p>U.S.Polo Assn.</p>
+                         <p>U.S.Polo Assn.Ivory Cotton</p>
+                         <p>Regular Fit Polo T-shirt</p>
+                         <p>Rs.<span>1899</span></p>
+           </div>
+           <div>
+            <img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017735385_437Wx649H_202305281615141.jpeg" alt="" />
+            <p>U.S.Polo Assn.</p>
+                         <p>U.S.Polo Assn.Ivory Cotton</p>
+                         <p>Regular Fit Polo T-shirt</p>
+                         <p>Rs.<span>1399</span></p>
+           </div>
+           <div>
+            <img src="https://img.tatacliq.com/images/i11/437Wx649H/MP000000017637148_437Wx649H_202305231340331.jpeg" alt="" />
+            <p>U.S.Polo Assn.</p>
+                         <p>U.S.Polo Assn.Ivory Cotton</p>
+                         <p>Regular Fit Polo T-shirt</p>
+                         <p>Rs.<span>1499</span></p>
+           </div>
+           <div>
+            <img src="https://img.tatacliq.com/images/i9/437Wx649H/MP000000016374385_437Wx649H_202302042111181.jpeg" alt="" />
+            <p>U.S.Polo Assn.</p>
+                         <p>U.S.Polo Assn.Ivory Cotton</p>
+                         <p>Regular Fit Polo T-shirt</p>
+                         <p>Rs.<span>1599</span></p>
+          
+           </div>
+           <div>
+            <img src="https://img.tatacliq.com/images/i9/437Wx649H/MP000000016374225_437Wx649H_202302042054181.jpeg" alt="" />
+            <p>U.S.Polo Assn.</p>
+                         <p>U.S.Polo Assn.Ivory Cotton</p>
+                         <p>Regular Fit Polo T-shirt</p>
+                         <p>Rs.<span>1699</span></p>
+           </div>
+
+
+        </div>
+       </div>
+       </>
+    )
+}
+export default MenSingle ;
